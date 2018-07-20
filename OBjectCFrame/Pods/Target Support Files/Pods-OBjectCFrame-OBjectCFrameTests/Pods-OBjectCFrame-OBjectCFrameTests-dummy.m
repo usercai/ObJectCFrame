dc9d32c32238a90c2262fd95f0253ea2dd128693
usercai <tenghongcai@163.com>
2018-07-20 15:03:18 +0800

    删除

--- OBjectCFrame/Pods/Target Support Files/Pods-OBjectCFrame-OBjectCFrameTests/Pods-OBjectCFrame-OBjectCFrameTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OBjectCFrame_OBjectCFrameTests : NSObject
-@end
-@implementation PodsDummy_Pods_OBjectCFrame_OBjectCFrameTests
-@end
--- OBjectCFrame/Pods/Target Support Files/Pods-OBjectCFrameUITests/Pods-OBjectCFrameUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OBjectCFrameUITests : NSObject
-@end
-@implementation PodsDummy_Pods_OBjectCFrameUITests
-@end
